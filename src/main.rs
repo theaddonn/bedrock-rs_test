ac398c9ec32230dd3a5b4e112a37e80870a05a21
Adrian <adrian.olbrich1@gmail.com>
2024-05-14 12:03:32 +0200

    adjust

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,191 @@
+use std::collections::HashMap;
+use std::io::{BufRead, Cursor};
 use std::net::SocketAddrV4;
 use std::path::Path;
+use std::process::exit;
 use std::str::FromStr;
+use bedrock_rs::nbt;
+use bedrock_rs::nbt::endian::little_endian_network::NbtLittleEndianNetwork;
+use bedrock_rs::nbt::endian::little_endian::NbtLittleEndian;
+
+use bedrock_rs::nbt::NbtTag;
+use bedrock_rs::nbt::NbtTag::Float64;
 
 use bedrock_rs::protocol::compression::CompressionMethods;
 use bedrock_rs::protocol::compression::zlib::ZlibCompression;
 use bedrock_rs::protocol::listener::ListenerConfig;
 use bedrock_rs::protocol::login::{handle_login_server_side, LoginServerSideOptions};
+use byteorder::{LittleEndian, ReadBytesExt};
 use tokio::main;
+
+use miniz_oxide::deflate::{compress_to_vec, compress_to_vec_zlib};
+use miniz_oxide::inflate::{decompress_to_vec, decompress_to_vec_zlib};
+use rusty_leveldb::compressor::NoneCompressor;
+use rusty_leveldb::{Compressor, CompressorList, Options, DB, LdbIterator};
+use std::rc::Rc;
+use input_macro::input;
+
+/// A zlib compressor that with zlib wrapper
+struct ZlibCompressor(u8);
+
+impl ZlibCompressor {
+    /// level 0-10
+    pub fn new(level: u8) -> Self {
+        assert!(level <= 10);
+        Self(level)
+    }
+}
+
+impl Compressor for ZlibCompressor {
+    fn encode(&self, block: Vec<u8>) -> rusty_leveldb::Result<Vec<u8>> {
+        Ok(compress_to_vec_zlib(&block, self.0))
+    }
+
+    fn decode(&self, block: Vec<u8>) -> rusty_leveldb::Result<Vec<u8>> {
+        decompress_to_vec_zlib(&block).map_err(|e| rusty_leveldb::Status {
+            code: rusty_leveldb::StatusCode::CompressionError,
+            err: e.to_string(),
+        })
+    }
+}
+
+/// A zlib compressor that without zlib wrapper
+///
+/// > windowBits can also be –8..–15 for raw deflate. In this case, -windowBits determines the window size. deflate() will then generate raw deflate data with no zlib header or trailer, and will not compute a check value.
+/// >
+/// > From [zlib manual](https://zlib.net/manual.html)
+///
+/// It seems like mojang use this most
+///
+/// A copy of mojang's implementation can be find [here](https://github.com/reedacartwright/rbedrock/blob/fb32a899da4e15c1aaa0d6de2b459e914e183516/src/leveldb-mcpe/db/zlib_compressor.cc#L119).
+struct RawZlibCompressor(u8);
+
+impl RawZlibCompressor {
+    /// level 0-10
+    pub fn new(level: u8) -> Self {
+        assert!(level <= 10);
+        Self(level)
+    }
+}
+
+impl Compressor for RawZlibCompressor {
+    fn encode(&self, block: Vec<u8>) -> rusty_leveldb::Result<Vec<u8>> {
+        Ok(compress_to_vec(&block, self.0))
+    }
+
+    fn decode(&self, block: Vec<u8>) -> rusty_leveldb::Result<Vec<u8>> {
+        decompress_to_vec(&block).map_err(|e| rusty_leveldb::Status {
+            code: rusty_leveldb::StatusCode::CompressionError,
+            err: e.to_string(),
+        })
+    }
+}
+
+pub fn mcpe_options(compression_level: u8) -> Options {
+    let mut opt = Options::default();
+
+    // compressor id list can be find in [mojang's implementation](https://github.com/reedacartwright/rbedrock/blob/fb32a899da4e15c1aaa0d6de2b459e914e183516/src/leveldb-mcpe/include/leveldb/c.h#L194-L200)
+    // And it seems like mojang don't use slippy or zstd of minecraft bedrock
+    //
+    // If you need to open old world, old world fallback compressor can be find [here](https://github.com/reedacartwright/rbedrock/blob/fb32a899da4e15c1aaa0d6de2b459e914e183516/src/bedrock_leveldb.c#L152-L153)
+    let mut list = CompressorList::new();
+    list.set_with_id(0, NoneCompressor::default());
+    list.set_with_id(2, ZlibCompressor::new(compression_level));
+    list.set_with_id(4, RawZlibCompressor::new(compression_level));
+    opt.compressor_list = Rc::new(list);
+
+    // Set compressor
+    // Minecraft bedrock may use other id than 4
+    //
+    // There is a bug in this library that you have to open a database with the same compression type as it was written to.
+    opt.compressor = 4;
+
+    opt
+}
 
 #[main]
 async fn main() {
+    // let data = include_bytes!("../Hoffnung/level.dat").to_vec();
+    // let mut cur = Cursor::new(data.clone());
+    //
+    // let (name, tag) = NbtTag::nbt_deserialize::<NbtLittleEndian>(&mut cur).unwrap();
+    // println!("DATA: {:#?}", (&name, &tag));
+    // cur.consume(8);
+    // let (name, tag) = NbtTag::nbt_deserialize::<NbtLittleEndian>(&mut cur).unwrap();
+    // println!("DATA: {:#?}", (&name, &tag));
+    // let mut buf = vec![];
+    //
+    // tag.nbt_serialize::<NbtLittleEndian>(name, &mut buf).unwrap();
+    //
+    // assert_eq!(data, buf);
+    //
+    // println!("SUCCESS!");
 
+    // let mut map = HashMap::new();
+    // map.insert("MEE".to_string(), NbtTag::String("AFRIKA".to_string()));
+    //
+    // let tag = NbtTag::Compound(map);
+    //
+    // let mut data = vec![];
+    //
+    // tag.nbt_serialize::<NbtLittleEndian>("NEW", &mut data).unwrap();
+    //
+    // let mut cur = Cursor::new(data.clone());
+    //
+    // let res = NbtTag::nbt_deserialize::<NbtLittleEndian>(&mut cur);
+    //
+    // println!("DATA: \n{res:#?}\n\n{tag:#?}");
+    // println!("  ");
+    // println!("DATA: \n{data:#?}");
+
+    // loop {
+    //
+    // }
+    //
+    //
+    // let path = "Hoffnung/db/";
+    // const COMPRESSION_LEVEL: u8 = 10;
+    // let opt = mcpe_options(COMPRESSION_LEVEL);
+    // let mut db = DB::open(path, opt).unwrap();
+    //
+    // let mut iter = db.new_iter().unwrap();
+    //
+    // let mut key = vec![];
+    // let mut val = vec![];
+    //
+    // loop {
+    //     iter.current(&mut key, &mut val);
+    //
+    //     let mut cur = Cursor::new(val.clone());
+    //     let res = NbtTag::nbt_deserialize::<NbtLittleEndianNetwork>(&mut cur);
+    //
+    //     println!("{:?}: {:?}", String::from_utf8_lossy(&*key), String::from_utf8_lossy(&*val));
+    //
+    //     println!("\nNBT\n{:#?}", res);
+    //
+    //     input!();
+    //
+    //     if !iter.advance() {
+    //         break;
+    //     };
+    // }
+    //
+    //
+    // exit(0);
+
+
+    //
+    // let mut cursor = Cursor::new(include_bytes!("../nbt_example_file.nbt").to_vec());
+    //
+    // let (mut name, mut tag) = NbtTag::nbt_deserialize::<NbtLittleEndian>(&mut cursor).unwrap();
+    //
+    // let mut buf = vec![];
+    //
+    // println!("{name:?}: {tag:#?}");
+    //
+    // NbtTag::nbt_serialize::<NbtLittleEndianNetwork>(&tag, name, &mut buf).unwrap();
+    //
+    // println!("INPUT:  {:?}\nOUTPUT: {:?}", include_bytes!("../nbt_example_file.nbt"), buf);
 
     let mut listener = bedrock_rs::protocol::listener::Listener::new(
         ListenerConfig {
