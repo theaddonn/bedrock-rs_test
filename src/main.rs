ab80b567fe87bcb66c465aa31a09451ae3d18ceb
Adrian <adrian.olbrich1@gmail.com>
2024-06-03 19:49:52 +0200

    adjust

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,7 @@ use std::io::Cursor;
 use std::net::SocketAddrV4;
 use std::str::FromStr;
 
-use bedrock_rs::proto::compression::CompressionMethods;
-use bedrock_rs::proto::compression::zlib::ZlibCompression;
+use bedrock_rs::proto::compression::Compression;
 use bedrock_rs::proto::listener::ListenerConfig;
 use bedrock_rs::proto::login::{handle_login_server_side, LoginServerSideOptions};
 use tokio::main;
@@ -19,12 +18,12 @@ async fn main() {
 
     println!("WORLD: \n{:#?}", world);
 
-    let mut listener = bedrock_rs::proto::listener::Listener::new(
+    let mut listener = bedrock_rs::proto::listener::Listener::new_raknet(
         ListenerConfig {
             name: String::from("My Server"),
             sub_name: String::from("bedrock-rs"),
-            player_count_max: u32::MAX,
-            player_count_current: u32::MAX-1,
+            player_count_max: i32::MAX as u32,
+            player_count_current: i32::MAX as u32 -1,
             nintendo_limited: false,
         },
         SocketAddrV4::from_str("127.0.0.1:19132").unwrap(),
@@ -39,7 +38,7 @@ async fn main() {
             println!("started!");
 
             let res_message = match handle_login_server_side(&mut conn, LoginServerSideOptions {
-                compression: CompressionMethods::Zlib(ZlibCompression { threshold: 1024, compression_level: 9 }),
+                compression: Compression::None,
                 encryption: false,
                 authentication_enabled: false,
                 allowed_proto_versions: vec![671, 662],
