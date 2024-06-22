7d98998e6ed8ab07f7104bf289063f2fe69ad769
Adrian <adrian.olbrich1@gmail.com>
2024-06-22 02:19:19 +0200

    adjust

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,35 @@
-use std::io::Cursor;
-use std::net::SocketAddrV4;
+use std::net::{SocketAddr, SocketAddrV4};
 use std::str::FromStr;
-
-use bedrock_rs::proto::compression::Compression;
-use bedrock_rs::proto::listener::ListenerConfig;
-use bedrock_rs::proto::login::{handle_login_server_side, LoginServerSideOptions};
-use tokio::main;
+use std::time::Duration;
 
 use bedrock_rs::core::*;
 use bedrock_rs::proto::gamepacket::GamePacket;
+use bedrock_rs::proto::login::handle::login_to_server;
+use bedrock_rs::proto::login::provider::DefaultLoginProvider;
 use bedrock_rs::proto::packets::disconnect::DisconnectPacket;
+use bedrock_rs::text::color;
+use tokio::main;
+
 //use bedrock_rs::world::World;
 
 #[main]
 async fn main() {
-
-
     //let world = World::open("C:/Users/adria/AppData/Local/Packages/Microsoft.MinecraftUWP_8wekyb3d8bbwe/LocalState/games/com.mojang/minecraftWorlds/cBFbZglOAQA=").unwrap();
+
+    let y = 6;
+    let x = color!("<red>Hello red, <bold>red bold</bold> Just red again</red> and now nothing lol");
+
+    println!("{x}");
 
     //println!("WORLD: \n{:#?}", world);
 
     let mut listener = bedrock_rs::proto::listener::Listener::new_raknet(
-        ListenerConfig {
-            name: String::from("My Server"),
-            sub_name: String::from("bedrock-rs"),
-            player_count_max: i32::MAX as u32,
-            player_count_current: i32::MAX as u32 -1,
-            nintendo_limited: false,
-        },
-        SocketAddrV4::from_str("127.0.0.1:19132").unwrap(),
+        String::from("My Server"),
+        String::from("bedrock-rs"),
+        10,
+        2,
+        SocketAddr::V4(SocketAddrV4::from_str("127.0.0.1:19132").unwrap()),
+        false,
     ).await.unwrap();
 
     listener.start().await.unwrap();
@@ -39,28 +40,21 @@ async fn main() {
         tokio::spawn(async move {
             println!("===== START =====");
 
-            let res_message = match handle_login_server_side(&mut conn, LoginServerSideOptions {
-                compression: Compression::None,
-                encryption: false,
-                authentication_enabled: false,
-                allowed_proto_versions: vec![671, 662],
-            }).await {
+            let mut conn = conn.into_shard(Duration::from_secs(1)).await;
+
+            let res_message = match login_to_server(&mut conn, DefaultLoginProvider::new()).await {
                 Ok(_) => { "success!".to_string() }
                 Err(e) => { format!("ERR({e:?})") }
             };
 
             println!("LOGIN RESULT: {:?}", res_message);
 
-            conn.send_gamepackets(vec![GamePacket::Disconnect(
+            conn.send(GamePacket::Disconnect(
                 DisconnectPacket {
                     reason: VAR::new(0),
                     message: Some(res_message),
                 }
-            )]).await.unwrap();
-
-            loop {
-
-            }
+            )).await.unwrap();
         });
     }
 }
